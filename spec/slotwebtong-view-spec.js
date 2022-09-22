91fdef5e1733f12be7f6536e13f059919927bb2a
Sushi <azhime.blog@gmail.com>
2022-09-23 04:46:41 +0700

    test 2
    2

---
+++ spec/slotwebtong-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SlotwebtongView from '../lib/slotwebtong-view';
+
+describe('SlotwebtongView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
